ffb5417e0305164b0ebab34c09e1ab7ed4a82eed
1337Bac9I <killerdipet1@gmail.com>
2026-02-26 02:51:43 -0900

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const User = require("./models/User");
 const express = require("express");
 const session = require("express-session");
 const mongoose = require("mongoose");
